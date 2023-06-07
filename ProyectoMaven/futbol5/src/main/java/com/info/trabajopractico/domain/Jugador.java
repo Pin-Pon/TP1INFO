73a59b2e02a6eccac2a4929a6f8bda947057272d
Tamara Hidalgo <hidalgotamaraandrea@gmail.com>
2023-06-07 10:24:27 -0300

    "Creando la clase Jugador con todos sus atributos, constructor , get-set, toString"

---
+++ ProyectoMaven/futbol5/src/main/java/com/info/trabajopractico/domain/Jugador.java
@@ -0,0 +1,129 @@
+package com.info.trabajopractico.domain;
+
+import java.util.UUID;
+
+public class Jugador {
+
+        private UUID id;
+        private String nombre;
+        private String apellido;
+        private double altura;
+        private String posicion;
+        private int goles;
+        private int partidos;
+        private boolean esCapitan;
+        private int numeroCamiseta;
+        private Equipo equipo;
+
+        public Jugador(UUID id, String nombre, String apellido, double altura, String posicion, int goles, int partidos, boolean esCapitan, int numeroCamiseta, Equipo equipo) {
+                this.id = id;
+                this.nombre = nombre;
+                this.apellido = apellido;
+                this.altura = altura;
+                this.posicion = posicion;
+                this.goles = goles;
+                this.partidos = partidos;
+                this.esCapitan = esCapitan;
+                this.numeroCamiseta = numeroCamiseta;
+                this.equipo = equipo;
+        }
+        public Jugador(){
+
+        }
+
+        public UUID getId() {
+                return id;
+        }
+
+        public void setId(UUID id) {
+                this.id = id;
+        }
+
+        public String getNombre() {
+                return nombre;
+        }
+
+        public void setNombre(String nombre) {
+                this.nombre = nombre;
+        }
+
+        public String getApellido() {
+                return apellido;
+        }
+
+        public void setApellido(String apellido) {
+                this.apellido = apellido;
+        }
+
+        public double getAltura() {
+                return altura;
+        }
+
+        public void setAltura(double altura) {
+                this.altura = altura;
+        }
+
+        public String getPosicion() {
+                return posicion;
+        }
+
+        public void setPosicion(String posicion) {
+                this.posicion = posicion;
+        }
+
+        public int getGoles() {
+                return goles;
+        }
+
+        public void setGoles(int goles) {
+                this.goles = goles;
+        }
+
+        public int getPartidos() {
+                return partidos;
+        }
+
+        public void setPartidos(int partidos) {
+                this.partidos = partidos;
+        }
+
+        public boolean isEsCapitan() {
+                return esCapitan;
+        }
+
+        public void setEsCapitan(boolean esCapitan) {
+                this.esCapitan = esCapitan;
+        }
+
+        public int getNumeroCamiseta() {
+                return numeroCamiseta;
+        }
+
+        public void setNumeroCamiseta(int numeroCamiseta) {
+                this.numeroCamiseta = numeroCamiseta;
+        }
+
+        public Equipo getEquipo() {
+                return equipo;
+        }
+
+        public void setEquipo(Equipo equipo) {
+                this.equipo = equipo;
+        }
+
+        @Override
+        public String toString() {
+                return "Jugador{" +
+                        "id=" + id +
+                        ", nombre='" + nombre + '\'' +
+                        ", apellido='" + apellido + '\'' +
+                        ", altura=" + altura +
+                        ", posicion='" + posicion + '\'' +
+                        ", goles=" + goles +
+                        ", partidos=" + partidos +
+                        ", esCapitan=" + esCapitan +
+                        ", numeroCamiseta=" + numeroCamiseta +
+                        ", equipo=" + equipo +
+                        '}';
+        }
+}
